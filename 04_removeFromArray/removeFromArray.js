83e15ed54f8de511538abfcb739b3e5cb5e345c2
andre starnman <andre.starnman@gmail.com>
2022-10-14 13:41:15 +0200

    jesttesting 5 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,6 @@
-const removeFromArray = function(myArray, b, c) { 
-    for (let i = 0; i < myArray.length; i++ ) {
-        return myArray;
-    }
-
-
-
+const removeFromArray = function (...args) {
+  const array = args[0];
+  return array.filter((val) => !args.includes(val));
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
