c9fe06405640e69d40b6fc4a89aed6ba12b81b2a
andre starnman <andre.starnman@gmail.com>
2022-10-15 18:04:11 +0200

    tempconversion done@

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
+const ftoc = function (farenheit) {
+  const celsius = (farenheit - 32) * 0.5556;
+  const rounded = Math.round(celsius * 10) / 10;
 
+  return rounded;
 };
 
-const ctof = function() {
+const ctof = function (celsius) {
+  const farenheit = celsius * 1.8 + 32;
+  const rounded = Math.round(farenheit * 10) / 10;
 
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
