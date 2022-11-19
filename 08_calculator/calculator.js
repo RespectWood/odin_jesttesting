3b88e8ce50f1e80cc75ee7fcff9bd102ef5f9c98
andre starnman <andre.starnman@gmail.com>
2022-11-19 14:38:35 +0100

    calculator functions done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,17 +8,31 @@ const subtract = function (a2, b2) {
 
 const sum = function (numbers) {
   let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
+  // for (let i = 0; i < numbers.length; i++)
+  for (const number of numbers) {
+    sum += number;
   }
   return sum;
 };
 
-const multiply = function () {};
+const multiply = function (multinumbers) {
+  let sum2 = multinumbers[0];
+  for (const number of multinumbers.slice(1)) {
+    sum2 *= number;
+  }
+  return sum2;
+};
 
-const power = function () {};
+const power = function (num1, num2) {
+  return num1 ** num2;
+};
 
-const factorial = function () {};
+const factorial = function (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
 
 // Do not edit below this line
 module.exports = {
