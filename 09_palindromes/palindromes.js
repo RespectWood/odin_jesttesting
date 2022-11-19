ac90e48d1b4443f2359a61295b1d8a74de0a9861
andre starnman <andre.starnman@gmail.com>
2022-11-19 16:48:28 +0100

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (incomingString = "") {
+  const cased = incomingString
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~ ()]/g, "");
+  const split = cased.split("");
+  const reversed = split.reverse();
+  const joined = reversed.join("");
+  return cased === joined;
 };
 
 // Do not edit below this line
